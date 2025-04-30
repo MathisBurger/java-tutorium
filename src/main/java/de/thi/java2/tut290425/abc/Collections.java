114112049128908137268f83b93ddf7a5f59351a
Mathis Burger <handymathis@gmail.com>
2025-04-30 11:20:46 +0200

    feat: Added collections

---
+++ src/main/java/de/thi/java2/tut290425/abc/Collections.java
@@ -0,0 +1,15 @@
+package de.thi.java2.tut290425.abc;
+
+import de.thi.java2.tut290425.uebung.Trecker;
+
+import java.util.*;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        Map<String, Trecker> treckers = new HashMap<>();
+        Set<String> keys = new HashSet<>();
+        List<Trecker> list = new LinkedList();
+        list.add(new Trecker());
+    }
+}
